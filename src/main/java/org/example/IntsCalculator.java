2a1ddbfbebe4519c50d7e0f2280558e380a733f5
Alexey Petin <alex_beholder@mail.ru>
2023-05-22 21:34:38 +0300

    Update1

--- src/main/java/org/example/IntsCalculator.java
+++ src/main/java/org/example/IntsCalculator.java
@@ -10,7 +10,7 @@ public class IntsCalculator implements Ints {
     @Override
     public int sum(int arg0, int arg1) {
         //считаем через target
-        target.newFormula()
+        return (int) target.newFormula()
                 .addOperand(arg0)
                 .addOperand(arg1)
                 .calculate(Calculator.Operation.SUM)
@@ -20,10 +20,20 @@ public class IntsCalculator implements Ints {
     @Override
     public int mult(int arg0, int arg1) {
         //считаем через target
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
     }
 
     @Override
     public int pow(int a, int b) {
         //считаем через target
+        return (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
     }
 }
